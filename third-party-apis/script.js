ca5f60a8938b102e45d6209751eb03a2a3a6d9e5
Manmohit Sehgal <sehgal.mani9@gmail.com>
2022-06-07 21:49:51 -0400

    code related to products

--- third-party-apis/script.js
+++ third-party-apis/script.js
@@ -1,18 +1,28 @@
-let promise = new Promise(function(resolve, reject) {
-  setTimeout(() => reject("I have fulfilled this promise!"), 3000);
-});
+// creating a promise
 
-promise
-  .then(function(result) {
-    alert(`Resolved Promise with value =  ${result}`);
-  })
-  .catch(function(error) {
-    alert(`Resolved Promise with error =  ${error}`);
-  })
-  .finally(function() {
-    console.log("This will run regardless");
+// const firstPromise = new Promise(function(resolve, reject) {
+//   // async event happens here
+//   setTimeout(() => reject("I have fulfilled this promise!"), 3000);
+// });
+
+// console.log("firstPromise", firstPromise);
+
+// firstPromise
+//   .then(result => alert(`Resolved Promise with value =  ${result}`))
+//   .catch(error => alert(`Resolved Promise with error =  ${error}`))
+//   .finally(always => alert("I am going to run no matter what"));
+
+/* ---------------------------------------------------------------------------------------------------------------------*/
+
+// fetch("https://dummyjson.com/products")
+//   .then(res => res.json()) // gets json
+//   .then(json => console.log(json)); // json is being used here
+
+const productsPromise = fetch("https://dummyjson.com/products");
+
+productsPromise
+  .then(val => val.json())
+  .then(jsonData => {
+    const productsArray = jsonData.products;
+    console.log(productsArray);
   });
-
-fetch("https://dummyjson.com/products/1")
-  .then(res => res.json())
-  .then(json => console.log(json));
