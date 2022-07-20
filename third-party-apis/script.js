c726b85e202759603c1a68d368f190d2284d25ff
Manmohit Sehgal <sehgal.mani9@gmail.com>
2022-07-19 21:23:33 -0400

    testing 2

--- third-party-apis/script.js
+++ third-party-apis/script.js
@@ -2,15 +2,37 @@ let promise = new Promise(function(resolve, reject) {
   setTimeout(() => reject("I have fulfilled this promise!"), 3000);
 });
 
-promise
-  .then(function(result) {
-    alert(`Resolved Promise with value =  ${result}`);
+// const firstPromise = new Promise(function(resolve, reject) {
+//   // async event happens here
+//   setTimeout(() => reject("I have fulfilled this promise!"), 3000);
+// });
+
+// console.log("firstPromise", firstPromise);
+
+// firstPromise
+//   .then(result => alert(`Resolved Promise with value =  ${result}`))
+//   .catch(error => alert(`Resolved Promise with error =  ${error}`))
+//   .finally(always => alert("I am going to run no matter what"));
+
+/* ---------------------------------------------------------------------------------------------------------------------*/
+
+// fetch("https://dummyjson.com/products")
+//   .then(res => res.json()) // gets json
+//   .then(json => console.log(json)); // json is being used here
+
+const productsPromise = fetch("https://dummyjson.com/products");
+
+productsPromise
+  .then(val => val.json())
+  .then(jsonData => {
+    const productsArray = jsonData.products;
+    console.log(productsArray);
   })
-  .catch(function(error) {
-    alert(`Resolved Promise with error =  ${error}`);
+  .catch(error => {
+    console.log(typeof error);
   })
-  .finally(function() {
-    console.log("This will run regardless");
+  .finally(() => {
+    console.log("this will always execute");
   });
 
 fetch("https://dummyjson.com/products/1")
